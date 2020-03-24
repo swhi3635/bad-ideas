ce48c42d32ac5f402ea795b5acb089c74eadaf18
Sam Whitney <sam.whitney@stonecroptech.com>
2020-03-24 10:39:13 -0700

    publish config changes, gitignore

--- blog/publishconf.py
+++ blog/publishconf.py
@@ -11,12 +11,13 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://bad-ideas.samwhitney.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
-
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
